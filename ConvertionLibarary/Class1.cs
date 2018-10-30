4bae8c1e4960191a4adf68a39dcf1fc1edccca2b
mans0ur7 <36192552+mans0ur7@users.noreply.github.com>
2018-10-30 09:52:43 +0100

    2 mehonds

--- ConvertionLibarary/Class1.cs
+++ ConvertionLibarary/Class1.cs
@@ -4,5 +4,14 @@ namespace ConvertionLibarary
 {
     public class Class1
     {
+        public double GramsToOunces(double Grams)
+        {
+            return Grams / 28.3495;
+        }
+
+        public double OuncesToGrams(double Ounces)
+        {
+            return Ounces * 28.3495;
+        }
     }
 }
